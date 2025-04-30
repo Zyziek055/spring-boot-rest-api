4854d4399c8d6c8aaeb7032d808f0428ba43e45c
Zyziek055 <sylgermroz@gmail.com>
2025-04-30 13:53:45 +0200

    Refactor User mapping logic to use UserMapper

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -3,8 +3,10 @@ package com.codewithmosh.store.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+
+
 @Getter
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String name;
---
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -0,0 +1,15 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    UserDto toDto(User user);
+}
+
+
