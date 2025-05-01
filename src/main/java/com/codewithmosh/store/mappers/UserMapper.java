a711c6ebfc66e7e87e14efe4acdafee89d98e0e7
Zyziek055 <sylgermroz@gmail.com>
2025-05-01 14:21:00 +0200

    -m

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -3,7 +3,6 @@ package com.codewithmosh.store.mappers;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 
