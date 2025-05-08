55e59df687687284ff0a43271cae169a7f2a9a9c
Zyziek055 <sylgermroz@gmail.com>
2025-05-08 18:02:21 +0200

    Handle validation errors in UserController

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -13,10 +13,13 @@ import org.apache.coyote.Response;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -106,4 +109,5 @@ public class UserController {
 
         return ResponseEntity.noContent().build();
     }
+
 }
